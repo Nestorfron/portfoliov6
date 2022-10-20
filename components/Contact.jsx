012a1c708649212eeb3d8c31e7e97cfde2e78819
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2022-10-20 19:56:13 -0300

    asdfds

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,10 +4,12 @@ import contactImg from '../public/assets/contact.jpg'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 import { AiOutlineMail } from 'react-icons/ai'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
+import Link from 'next/link'
+import { HiChevronDoubleUp } from 'react-icons/hi'
 
 const Contact = () => {
   return (
-    <div className='w-full lg:h-screen'>
+    <div id='contact' className='w-full lg:h-screen'>
         <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Contact</p>
             <h2 className='py-4'>Get In Touch</h2>
@@ -64,11 +66,19 @@ const Contact = () => {
                 </div>
                 <div className='flex flex-col py-2'>
                     <label className='uppercase text-sm py-2'>Message</label>
-                    <textarea className='border-2 rounded-lg p-3 flex border-gray-300' type="tex t"/>
+                    <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
                 </div>
+                <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
             </form>
                      </div>
                  </div>
+            </div>
+            <div className='flex justify-center p-12'>
+                <Link href='/'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-500'>
+                        <HiChevronDoubleUp className='text-[#5651e5]' size={30}/>
+                    </div>
+                </Link>
             </div>
         </div>
     </div>
--- components/Main.jsx
+++ components/Main.jsx
@@ -8,7 +8,7 @@ import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
 
 const Main = () => {
   return (
-    <div className='w-full h-screen text-center'>
+    <div id='home' className='w-full h-screen text-center'>
         <div className='max-w-[1240px] h-full mx-auto p-2 flex justify-center items-center'>
             <div>
                 <p className='upercase text-sm tracking-widest text-gray-600'>LET´S BUILD SOMETHING TOGETHER</p>
