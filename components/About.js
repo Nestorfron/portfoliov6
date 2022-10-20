c98a3b06e2953d490d1abb38a7bfa4c5b92115ca
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2022-10-20 14:32:49 -0300

    fghjdj

--- components/About.js
+++ components/About.js
@@ -6,7 +6,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full md:h-screen p-2 flex items-center py-16 '>
+        <div className='max-w-[1240px] m-auto md:ftid ftid-cols-3 gap-8'>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
   )
 }
 
