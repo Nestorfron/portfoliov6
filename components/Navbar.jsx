0e0c57466511c6f0320bf42d521c5bdce42af2e2
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2022-10-20 20:02:28 -0300

    gadg

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -81,19 +81,19 @@ const Navbar = () => {
                     <div className='py-4 flex-col'>
                         <ul>
                             <Link href='/'>
-                                <li className='py-4 text-sm'>Home</li>
+                                <li onClick={()=>setNav(true)} className='py-4 text-sm'>Home</li>
                             </Link>
-                            <Link href='/'>
-                                <li className='py-4 text-sm'>About</li>
+                            <Link href='/#about'>
+                                <li onClick={()=>setNav(true)} className='py-4 text-sm'>About</li>
                             </Link>
-                            <Link href='/'>
-                                <li className='py-4 text-sm'>Skills</li>
+                            <Link href='/#skills'>
+                                <li onClick={()=>setNav(true)} className='py-4 text-sm'>Skills</li>
                             </Link>
-                            <Link href='/'>
-                                <li className='py-4 text-sm'>Projects</li>
+                            <Link href='/#projects'>
+                                <li onClick={()=>setNav(true)} className='py-4 text-sm'>Projects</li>
                             </Link>
-                            <Link href='/'>
-                                <li className='py-4 text-sm'>Contact</li>
+                            <Link href='/#contact'>
+                                <li onClick={()=>setNav(true)} className='py-4 text-sm'>Contact</li>
                             </Link>
                         </ul>
                         <div className='pt-40'>
---
+++ pages/property.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const property = () => {
+  return (
+    <div>propety</div>
+  )
+}
+
+export default property
