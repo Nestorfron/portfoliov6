f4eee3754c450b75a637943212012776c8114585
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2022-10-20 19:19:10 -0300

    gsdfg

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -22,25 +22,53 @@ const Contact = () => {
             <p>Front-End Developer</p>
             <p  className='py-4'>I am available for freelance or full-time positions. Contact me and let´s talk.</p>
         </div>
-<div>
-    <p className='uppercase pt-8'>Connect With Me</p>
-    <div className='flex items-center justify-between py-4'>
-        <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                <FaLinkedinIn /> 
-            </div>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                <FaGithub /> 
-            </div>   
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                <AiOutlineMail /> 
-            </div>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
-                <BsFillPersonLinesFill /> 
-        </div>
-    </div>
+        <div>
+            <p className='uppercase pt-8'>Connect With Me</p>
+            <div className='flex items-center justify-between py-4'>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+                        <FaLinkedinIn /> 
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+                        <FaGithub /> 
+                    </div>   
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+                        <AiOutlineMail /> 
+                    </div>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-500'>
+                        <BsFillPersonLinesFill /> 
+                    </div>
+</div>
 </div>      
 </div> 
 </div>
+    <div className='col-span-3 w-full h-auto shadow-xl stroke-gray-400 rounded-xl lg:p-4'>
+        <div className='p-4'>
+            <form>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                    <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Name</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" required/>
+                    </div>
+                    <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Phone Number</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" required/>
+                    </div>
+                </div>
+                <div className='flex flex-col py-2'>
+                    <label className='uppercase text-sm py-2'>Email</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="email" required/>
+                </div>
+                <div className='flex flex-col py-2'>
+                    <label className='uppercase text-sm py-2'>Subjet</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type="text" />
+                </div>
+                <div className='flex flex-col py-2'>
+                    <label className='uppercase text-sm py-2'>Message</label>
+                    <textarea className='border-2 rounded-lg p-3 flex border-gray-300' type="tex t"/>
+                </div>
+            </form>
+                     </div>
+                 </div>
             </div>
         </div>
     </div>
