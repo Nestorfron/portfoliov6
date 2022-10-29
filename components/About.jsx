e1a2d46003700f65e27dcd7d896530013a8999f1
Nestor Frones <115091611+Nestorfron@users.noreply.github.com>
2022-10-29 18:13:07 -0300

    dghjeghj

--- components/About.jsx
+++ components/About.jsx
@@ -12,20 +12,13 @@ const About = () => {
             <div className='col-span-2'>
               <p className='uppercase text-xl tracking-widest text-[#4791C2]'>About</p>
               <h2 className='py-4'>Who I am</h2>
-              <p className='py-2  text-gray-600'>I am not your normal developer</p>
-              <p className='py-2  text-gray-600'>I have spent the last 12 years in the fire service working as a professional fireligter &I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a 
-                parametric. I havve alwais had a 
+              <h6 className='py-2  text-gray-600'>I am not your normal developer</h6>
+              <p className='py-2  text-gray-600'>I enjoy taking complex problem and turning them into simple and beautiful interface design.
+              i also love the logic and estructure of coding and always strive to write elegant and efficient code, hether it be HTML, CSS or jQuery.
               </p>
               <p className='py-2  text-gray-600'>
-              I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a I have spent the last 12 years in the fire service working as a professional fireligter &
-                parametric. I havve alwais had a 
+              I spent the last 17 years working as a Police officer knowing that my true vocation is the world of software. 
+              Finally, the door that I needed to be able to study and become a professional in this world full of opportunities opened for me.
               </p>
               <a href='#projects'  className='py-2  text-gray-600 underline cursor-pointer'>Check out some of my latest projects.</a>
             </div>
